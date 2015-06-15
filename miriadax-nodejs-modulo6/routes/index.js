7507fe9bd796aedba15c97eac06dfb80249f4f20
Alberto Pascual <apascual93@gmail.com>
2015-06-15 11:28:16 +0200

    Añadido creditos de usuario

--- miriadax-nodejs-modulo6/routes/index.js
+++ miriadax-nodejs-modulo6/routes/index.js
@@ -7,6 +7,11 @@ var quizController = require('../controllers/quiz_controller');
  router.get('/', function(req, res) {
    res.render('index', { title: 'Quiz' });
  });
+ /* GET Author page. */
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Quiz' });
+});
+
  
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer',   quizController.answer);
